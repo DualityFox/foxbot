ae8f2a6900a3a6434b3b28a7f72ca80408f1a01f
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-02-24 16:17:41 +0100

    Update unwarn.js

--- commands/unwarn.js
+++ commands/unwarn.js
@@ -5,10 +5,10 @@ module.exports = {
         const guild = message.guild;
         if (!message.member.hasPermission('VIEW_AUDIT_LOG')) return message.channel.send("Tu n'as pas la permission ``Voir les logs du serveur``.")
         const member = message.mentions.members.first()
-        if (!member) return message.channel.send("Merci de mentionner le membre à unwarn\nUtilisation : ``db/unwarn @mention``")
+        if (!member) return message.channel.send("Merci de mentionner le membre à unwarn\nUtilisation : ``fb!unwarn [@mention] [numéro du warn]``")
         if (!client.db.warns[member.id+guild.id]) return message.channel.send("Ce membre n'a aucun avertissement")
         const warnIndex = parseInt(args[1], 10) - 1
-        if (warnIndex < 0 || !client.db.warns[member.id+guild.id][warnIndex]) return message.channel.send("Merci de présicer le numéro du warn\nUtilisation : ``db/unwarn @mention <numéro du warn>``\n\nSi cela fonctionne toujours pas il ce peut que le membre mentionné n'est pas autant de warn\n\nVérifie avec ``dbd/infrations @mention``")
+        if (warnIndex < 0 || !client.db.warns[member.id+guild.id][warnIndex]) return message.channel.send("Merci de présicer le numéro du warn\nUtilisation : ``fb!unwarn [@mention] [numéro du warn]``\n\nSi cela fonctionne toujours pas il ce peut que le membre mentionné n'est pas autant de warn\n\nVérifie avec ``fb!infrations [@mention]``")
         const { reason } = client.db.warns[member.id+guild.id].splice(warnIndex, 1)[0]
         if (!client.db.warns[member.id+guild.id].length) delete client.db.warns[member.id+guild.id]
         fs.writeFileSync("./db.json", JSON.stringify(client.db))
@@ -17,7 +17,7 @@ module.exports = {
     name: 'unwarn',
     guildOnly: true,
     help : {
-        description: "Supprime un avertissement du membre mmentionné",
+        description: "Supprime un avertissement du membre mentionné",
         syntax: "[@mention] [numéro du warn]"
     }
 }
