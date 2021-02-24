165f66c4a5b9998682b3186eab9653f5dd8e3db8
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-02-24 16:38:59 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,12 +2,12 @@ const Discord = require("discord.js")
 
 module.exports = {
     run: async (message, args) => {
-        if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send("Tu n'as pas la permission ``Bannir des membres``.")
+        if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply("tu n'as pas la permission ``Bannir des membres``.")
         const member = message.mentions.members.first()
-        if (!member) return message.channel.send("Merci de mentionner le membre à bannir\nUtilisation : ``fb!ban [@mention] <raison>``")
-        if (member.id === message.guild.ownerID) return message.channel.send("On ne banni pas le propriétaire du serveur voyons !!!")
-        if(message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send("Vous ne pouvez pas bannir ce membre")
-        if(!member.bannable) return message.channel.send("Mon rôle n'est pas assez élevé pour bannir ce membre !")
+        if (!member) return message.reply("merci de mentionner le membre à bannir\nUtilisation : ``fb!ban [@mention] <raison>``")
+        if (member.id === message.guild.ownerID) return message.reply("On ne banni pas le propriétaire du serveur voyons !!!")
+        if(message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.reply("Vous ne pouvez pas bannir ce membre")
+        if(!member.bannable) return message.reply("Mon rôle n'est pas assez élevé pour bannir ce membre !")
         const reason = args.slice(1).join(" ") || 'Aucune raison fournie'
         await member.ban({reason})
         message.channel.send( new Discord.MessageEmbed()
