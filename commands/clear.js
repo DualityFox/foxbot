b87586c6e51929493d937d9e17157a621c61c13d
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-02-24 16:12:33 +0100

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -4,8 +4,8 @@ module.exports = {
     run: async (message, args) => {
         if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Vous n'avez pas la permission ``Gérer les messages``")
         const count = args[0]
-        if (!/\d+/.test(count)) return message.channel.send("Merci d'indiquer le nombre de messages à supprimer\nUtilisation : ``db/clear <nombre entre 1 et 99>``")
-        if (count < 1 || count > 99) return message.channel.send("Le nombre de message doit être compris entre 1 et 99\nUtilisation : ``dbd/clear <nombre entre 1 et 99>``")
+        if (!/\d+/.test(count)) return message.channel.send("Merci d'indiquer le nombre de messages à supprimer\nUtilisation : ``fb!clear [nombre entre 1 et 99]``")
+        if (count < 1 || count > 99) return message.channel.send("Le nombre de message doit être compris entre 1 et 99\nUtilisation : ``fb!clear [nombre entre 1 et 99]``")
         const { size } = await message.channel.bulkDelete(Number(count) + 1, true)
         message.channel.send(`${size - 1} messages ont été supprimés !`).then(sent => sent.delete({timeout: 5e3}))
     },
