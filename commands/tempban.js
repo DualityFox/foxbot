9de4ab44399d24c0588a7c47cdfd98bdfc858fe2
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-02-24 16:08:33 +0100

    Update tempban.js

--- commands/tempban.js
+++ commands/tempban.js
@@ -6,7 +6,7 @@ module.exports = {
     run: async (message, args) => {
         if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send("Tu n'as pas la permission ``Bannir des membres``.")
         const member = message.mentions.members.first()
-        if (!member) return message.channel.send("Merci de mentionner le membre à bannir temporairement\nUtilisation : ``db/tempban @mention <durée> <raison>``")
+        if (!member) return message.channel.send("Merci de mentionner le membre à bannir temporairement\nUtilisation : ``fb!tempban [@mention] [durée] <raison>``")
         if (member.id === message.guild.ownerID) return message.channel.send("On ne banni pas le propriétaire du serveur voyons !!!")
         if(message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send("Vous ne pouvez pas exclure ce membre")
         if(!member.bannable) return message.channel.send("Mon rôle n'est pas assez élevé pour bannir temporairement ce membre !")
