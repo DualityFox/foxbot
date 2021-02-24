2a6bfc48a5a687edc152e2173660d7ac05635d0c
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-02-24 16:19:00 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -5,11 +5,11 @@ module.exports = {
         const guild = message.guild;
         if (!message.member.hasPermission('VIEW_AUDIT_LOG')) return message.channel.send("Tu n'as pas la permission ``Voir les logs du serveur``.")
         const member = message.mentions.members.first()
-        if (!member) return message.channel.send("Merci de mentionner le membre à warn\nUtilisation : ``db/warn @mention <raison>``")
+        if (!member) return message.channel.send("Merci de mentionner le membre à warn\nUtilisation : ``fb!warn [@mention] [raison]``")
         if (member.id === message.guild.ownerID) return message.channel.send("On ne warn pas le propriétaire du serveur voyons !!!")
         if(message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send("Vous ne pouvez pas warn ce membre")
         const reason = args.slice(1).join(" ")
-        if (!reason) return message.channel.send("Merci de donner une raison !\nUtilisation : ``dbd/warn @mention <raison>``")
+        if (!reason) return message.channel.send("Merci de donner une raison !\nUtilisation : ``fb!warn [@mention] [raison]``")
         if (!client.db.warns[member.id+guild.id]) client.db.warns[member.id+guild.id] = []
         client.db.warns[member.id+guild.id].unshift({
             reason,
