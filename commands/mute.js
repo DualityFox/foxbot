e1e81f79f970777e21c3be59df560e8652f6ecae
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-02-24 16:14:08 +0100

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -4,12 +4,12 @@ module.exports = {
     run: async (message, args) => {
         if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("Vous n'avez pas la permission ``Administrateur``")
         const member = message.mentions.members.first()
-        if (!member) return message.channel.send('Veuillez mentionner le membre à mute.\nUtilisation : ``fb!mute @mention <raison>``')
+        if (!member) return message.channel.send('Veuillez mentionner le membre à mute.\nUtilisation : ``fb!mute [@mention] <raison>``')
         if (member.id === message.guild.ownerID) return message.channel.send('On ne mute pas le propriétaire du serveur voyons.')
         if (message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send('Vous ne pouvez pas mute ce membre.')
         if (!member.manageable) return message.channel.send("Mon rôle n'est pas assez élevé pour mute ce membre.")
         const reason = args.slice(1).join(' ') || 'Aucune raison fournie'
-        let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted by DualityBot')
+        let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted by •|Fox Bot|•')
         if (!muteRole) {
             muteRole = await message.guild.roles.create({
                 data: {
