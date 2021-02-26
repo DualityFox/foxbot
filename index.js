e7cd28e03236391dae010041fdd82c52bb6afad6
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-02-26 09:59:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ client.on( 'ready', () => {
     client.user.setStatus("dnd")
     const statuses = [
         () => `•|Fox Graff|•`,
-        () => `les ${client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0) - 25} membres sur •|Fox Graff|•`
+        () => `les ${client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0) - 26} membres sur •|Fox Graff|•`
     ]
     let i = 0
     setInterval(() => {
