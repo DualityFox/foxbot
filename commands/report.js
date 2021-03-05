5b8fbb304615a39c58b70c63d18a8cbdcd2025fc
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-03-05 16:55:05 +0100

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -6,9 +6,9 @@ module.exports = {
         message.delete();
  
         let target = message.mentions.members.first()
-        if(!target) return message.channel.send("Merci de mentionner un utilisateur").then(sent => sent.delete({timeout: 5e3}))
+        if(!target) return message.channel.send("Merci de mentionner un utilisateur\nUtilisation : ``fb!report [@mention] [plainte]``").then(sent => sent.delete({timeout: 5e3}))
         let reason = args.slice(1).join(" ");
-        if(!reason) return message.channel.send(`Merci de bien préciser la plainte pour pouvoir porter plainte contre un utilisateur **${target.user.tag}**`).then(sent => sent.delete({timeout: 5e3}))
+        if(!reason) return message.channel.send(`Merci de bien préciser la plainte pour pouvoir porter plainte contre l\'utilisateur **${target.user.tag}**\nUtilisation : \`fb!report [@mention] [plainte]\`.`).then(sent => sent.delete({timeout: 5e3}))
      
      
         message.channel.send("Votre plainte à été envoyer au fondateurs et modérateurs, ils régleront sa le plus vite possible.").then(sent => sent.delete({timeout: 5e3}))
