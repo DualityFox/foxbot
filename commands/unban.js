5fbf7950048d1235633e265e1dc01eadeef1ce31
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-03-08 11:19:57 +0100

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -5,6 +5,7 @@ module.exports = {
         if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send("Tu n'as pas la permission ``Bannir des membres``.")
         let member = await client.users.fetch(args[0]);
         if (!member) return message.channel.send("L'utilisateur n'existe pas où plus");
+        if (!member.ban) return message.channel.send("L'utilisateur n'est pas banni du serveur");
         message.guild.members.unban(member)
         message.channel.send(`**${member.username}** à été débanni`)
     },
