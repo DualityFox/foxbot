54c153ce34b91e6ae2aee27123dc6a1b3ebfe053
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-03-08 10:26:03 +0100

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -5,7 +5,7 @@ const config = require('../config.json'),
 module.exports = {
     run: async (message, args, client) => {
         if (Object.values(client.db.tickets).some(ticket => ticket.author === message.author.id)) return message.channel.send('Vous avez déjà un ticket d\'ouvert.')
-        const channel = await message.guild.channels.create(`ticket ${message.author.username}`, {
+        const channel = await message.guild.channels.create(`📝・ticket ${message.author.username}`, {
             type: 'text',
             parent: config.ticket.category,
             permissionOverwrites: [{
