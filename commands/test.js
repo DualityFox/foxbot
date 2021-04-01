9bc06ea3f2433fa0090c14f382a6ecb79656461c
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-04-01 08:33:57 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,12 +1,19 @@
 const Discord = require("discord.js")
+const config = require("../config.json")
 
 module.exports = {
-    run: message => {
-        message.channel.send( new Discord.MessageEmbed()
-        .setTitle('Liste des commandes')
-        .addField('.', `${client.commands.filter(command => command.help).map(command => `\`${command.name}\``).join(' ')}`)
-        .setDescription(`Pour plus d'info sur une commande tapez \`${config.prefix}help [nom de la commande]\``)
-        .setFooter(`Bot crée par DualityFox#0540`))   
+    run: (message, args, client) => {
+        message.delete();
+ 
+        let message = args.slice(1).join(" ");
+        if(!message) return message.channel.send(`Erreur`)
+     
+     
+        message.channel.send(`${message}`)
     },
-    name: 'test'
+    name: 'test',
+    guildOnly: true,
+    help : {
+        description: "test"
+    }
 }
