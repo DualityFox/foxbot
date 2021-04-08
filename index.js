610d1546135f3d72a0dfb1ab1c67b55b6b8906cb
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-04-08 17:40:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ client.on( 'ready', () => {
     setInterval(() => {
         client.user.setActivity(statuses[i](), {type: `WATCHING`,})
         i = ++i % statuses.length
-    },1e2)
+    },1e6)
 })
 
 client.on('channelCreate', channel => {
