a0cfebd477498690f1c1eb5a88dd2d1defeda475
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-06-01 16:30:58 +0200

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -4,6 +4,7 @@ module.exports = {
     run: async (message, args, client) => {
         const reason = args.slice(1).join(" ")
         message.mentions.members.first().createDM().then(channel => {channel.send(`${reason}`)});
+        return message.channel.send(`Le message à bien était envoyé à **${message.mentions.members.first().user.username}** !`)
     },
     name: 'dm',
     guildOnly: true,
