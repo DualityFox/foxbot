a3f686d22eb1dea1462b624492fe5f81ad940f67
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-06-09 15:54:59 +0200

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js")
-
+config = require('../config.json')
 module.exports = {
     run: async (message, args, client) => {
         if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send("Tu n'as pas la permission ``Bannir des membres``.")
@@ -8,6 +8,12 @@ module.exports = {
         if (!member.ban) return message.channel.send("L'utilisateur n'est pas banni du serveur");
         message.guild.members.unban(member)
         message.channel.send(`**${member.username}** à été débanni`)
+        message.guild.channels.cache.get(config.logs).send(new Discord.MessageEmbed()
+        .setAuthor(`[UNBAN] ${member.username}`)
+        .setColor('#ff0000')
+        .addField('Utilisateur', member, true)
+        .addField('Modérateur', message.author, true)
+        )
     },
     name: 'unban',
     guildOnly: true,
