50b2ee89c0b70da6da09c0b8f5e08011597f764d
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-07-08 15:08:19 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -15,49 +15,49 @@ module.exports = {
             const page1 = new Discord.MessageEmbed()
             .setTitle("Liste des commandes")
             .setDescription(`Pour plus d'info sur une commande tapez \`${config.prefix}help [nom de la commande]\``)
-            .addField("Sommaire", "Page 1 : ``Sommaire``\nPage 2 : ``⚙️Administration``\nPage 3 : ``🎉Amusement``\nPage 4 : ``📸 Image``\nPage 5 : ``🎵 Musique``\nPage 6 : ``📡 Utilitaire``\nPage 7 : ``📌Autre``\nPage 8 : ``🔗 Lien utile``")
+            .addField("Sommaire", "Page 1 : **Sommaire**\nPage 2 : **⚙️Administration**\nPage 3 : **🎉Amusement**\nPage 4 : **📸 Image**\nPage 5 : **🎵 Musique**\nPage 6 : **📡 Utilitaire**\nPage 7 : **📌Autre**\nPage 8 : **🔗 Lien utile**")
             .setColor('#142c96')
             .setTimestamp();
 
             const page2 = new Discord.MessageEmbed()
             .setTitle('⚙️ Administration :')
             .setDescription("Voici toutes les commandes d'administration")
-            .addField('Liste des commandes', '``ban``・``clear``・``kick``・``mute``・``tempban``・``tempmute``・``unmute``・``warn``・``unwarn``・``infractions``',true)
+            .addField('Liste des commandes', '**ban**: Banni l\'utilisateur mentionée\n\n**clear**: Supprime le nombre de message demander\n\n**kick**: Expulse le membre mentionné\n\n**mute**: Rend muet le membre mentionné\n\n**tempban**: Banni le membre mentionné pendant le temps donné\n\n**tempmute**: Rend muet le membre mentionné pendant le temps donné\n\n**unmute**: Réautorise le membre mentionné à parler\n\n**warn**: Avertit le membre mentionné *(lui envoie un message privé)*\n\n**unwarn**: Retire un warn au membre mentionné\n\n**infractions**: Affiche les infractions du membre mentionné',true)
             .setColor('#ff0707')
             .setTimestamp();
 
             const page3 = new Discord.MessageEmbed()
             .setTitle("🎉 Amusement")
             .setDescription("Voici toutes les commandes d'amusement")
-            .addField('Liste des commandes', '``hug``・``kiss``・``pat``・``slap``・``smug``・``tickle``・``feed``・``baka``・``8ball``', true)
+            .addField('Liste des commandes', '**hug**: Faire un calin au membre mentionné\n\n**kiss**: Faire un bisous au membre mentionné\n\n**pat**: Tapoter le membre mentionné\n\n**slap**: Frapper le membre mentionné\n\n**smug**: Sourire où sourire au membre mentionné\n\n**tickle**: Chatoullier le membre mentionné\n\n**feed**: Manger ou donné à manger au membre mentionné\n\n**baka**: Insulter le membre mentionné\n\n**8ball**: Posser une question le bot vous répond', true)
             .setColor('#fab701')
             .setTimestamp();
 
             const page4 = new Discord.MessageEmbed()
             .setTitle("📸 Image")
             .setDescription("Voici toutes les commandes d'image")
-            .addField('Liste des commandes','``fox_girl``・``neko``・``waifu``・``meow``', true)
+            .addField('Liste des commandes','**fox_girl**: Envoie une image d\'une fille renarde\n\n**neko**: Envoie une image d\'une fille d\'animée\n\n**waifu**: Envoie une image d\'une fille d\'animée mignone\n\n**meow**: Miaou', true)
             .setColor('#5a127e')
             .setTimestamp();
 
             const page5 = new Discord.MessageEmbed()
             .setTitle("🎵 Musique")
             .setDescription("Voici toutes les commandes de musique")
-            .addField('Liste des commandes', '``play``・``disconnect``', true)
+            .addField('Liste des commandes', '**play**: Demarre la musique demander\n\n**disconnect**: Stoppe la musique', true)
             .setColor('#216032')
             .setTimestamp();
 
             const page6 = new Discord.MessageEmbed()
             .setTitle("📡 Utilitaire")
             .setDescription("Voici toutes les commandes utile")
-            .addField('Liste des commandes','``ticket``・``close``・``report``', true)
+            .addField('Liste des commandes','**ticket**: Ouvre un ticket à votre nom\n\n**close**: Ferme votre ticket\n\n**report**: Avertir les modérateurs d\'une fautes commis par quelqu\'un', true)
             .setColor('#0e0e0e')
             .setTimestamp();
 
             const page7 = new Discord.MessageEmbed()
             .setTitle("📌 Autre")
             .setDescription("Voici toutes les autres commandes")
-            .addField('Liste des commandes','``help``・``botinfo``・``serverinfo``・``userinfo``', true)
+            .addField('Liste des commandes','**help**: Cette commande même\n\n**botinfo**: Donne des informations sur le bot\n\n**serverinfo**: Donne des informations sur le serveur\n\n**userinfo**: Donne des informations sur l\'utilisateur mentionné', true)
             .setColor('#acaeac')
             .setTimestamp();
 
