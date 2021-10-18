d77a23181f356d936278baa49be4cdc1038de481
DualityFox <66992147+DualityFox@users.noreply.github.com>
2021-10-18 16:11:50 +0200

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -4,30 +4,30 @@ const config = require('../config.json'),
  
 module.exports = {
     run: async (message, args, client) => {
-          return message.channel.send("__**Désolé !**__\nLes tickets ont étaient temporairement désactivé"");
-       // if (Object.values(client.db.tickets).some(ticket => ticket.author === message.author.id)) return message.channel.send('Vous avez déjà un ticket d\'ouvert.')
-       //const channel = await message.guild.channels.create(`📝・ticket ${message.author.username}`, {
-         //   type: 'text',
-           // parent: config.ticket.category,
-         //   permissionOverwrites: [{
-           //     id: message.guild.id,
-             //   deny: 'VIEW_CHANNEL'
-            //}, {
-              //  id: message.author.id,
-                //allow: 'VIEW_CHANNEL'
-           //}, ...config.ticket.roles.map(id => ({
-              //  id,
-                //allow: 'VIEW_CHANNEL'
-            //}))]
-        //})
-      //  client.db.tickets[channel.id] = {
-          //  author: message.author.id
-        //}
-        //fs.writeFileSync('./db.json', JSON.stringify(client.db))
-        //channel.send(`Bonjour ${message.member}, bienvenue dans votre ticket.`)
-        //channel.send( new Discord.MessageEmbed()
-        //.setDescription('Pour fermer ce ticket tape ``fb!close`` .'))
-        //message.channel.send(`Votre ticket ${channel} a été créé !`)
+         
+        if (Object.values(client.db.tickets).some(ticket => ticket.author === message.author.id)) return message.channel.send('Vous avez déjà un ticket d\'ouvert.')
+        const channel = await message.guild.channels.create(`📝・ticket ${message.author.username}`, {
+            type: 'text',
+            parent: config.ticket.category,
+            permissionOverwrites: [{
+                id: message.guild.id,
+                deny: 'VIEW_CHANNEL'
+            }, {
+                id: message.author.id,
+                allow: 'VIEW_CHANNEL'
+           }, ...config.ticket.roles.map(id => ({
+                id,
+                allow: 'VIEW_CHANNEL'
+            }))]
+        })
+        client.db.tickets[channel.id] = {
+            author: message.author.id
+        }
+        fs.writeFileSync('./db.json', JSON.stringify(client.db))
+        channel.send(`Bonjour ${message.member}, bienvenue dans votre ticket.`)
+        channel.send( new Discord.MessageEmbed()
+        .setDescription('Pour fermer ce ticket tape ``fb!close`` .'))
+        message.channel.send(`Votre ticket ${channel} a été créé !`)
        }
     name: 'ticket',
     guildOnly: true,
